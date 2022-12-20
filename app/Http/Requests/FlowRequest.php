1059b5fc588750f57c61a47a663bb0af30e561da
pablolucas890 <pablolucas890@gmail.com>
2022-12-20 13:04:04 -0300

    Create a route for receive flows and send to specific users

---
+++ app/Http/Requests/FlowRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class FlowRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "all_users" => "sometimes|boolean",
+            "id_empresa" => "sometimes|numeric",
+            "nome_flow" => "sometimes|max:50",
+            "id_flow" => "sometimes|numeric",
+        ];
+    }
+    /**
+     * Get the error messages for the defined validation rules.*
+     * @return array
+     */
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors()
+        ], 422));
+    }
+}
