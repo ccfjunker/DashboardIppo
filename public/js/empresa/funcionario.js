b6c3719d8f351919906422a1c2bb1dbe2311726d
pablolucas890 <pablolucas890@gmail.com>
2023-01-03 10:24:32 -0300

    Delete Funcionario: Change id_empresa to default (Sideout)

--- public/js/empresa/funcionario.js
+++ public/js/empresa/funcionario.js
@@ -25,4 +25,12 @@ export class Funcionario{
             $('#funcionarioTable').DataTable().ajax.reload();
         });
     }
+
+    static delete(id){
+        let ippoAjax = new IppoAjaxJSON("/deletarFuncionario/"+id, "GET", "funcionario.delete").createRequest(null);
+        ippoAjax.done(function (data){
+            alert(data)
+            window.location.reload();
+        });
+    }
 }
